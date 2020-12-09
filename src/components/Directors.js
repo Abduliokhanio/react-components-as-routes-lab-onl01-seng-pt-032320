bf76df4bbb4b1e83ceb63500340836379e9523de
Abdul Khan <akhan17@outlook.com>
2020-12-09 19:41:06 +0000

    Automatically backed up by Learn

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,9 +2,20 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
+  const directorDivs = directors.map((director, index) => {
+    return (
+      <div key={index}>
+        {director.name}
+        <ul>
+          {moviesLi(director.movies)}
+        </ul>
+      </div>
+    )
+  })
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directorDivs}
     </div>
   );
 }
